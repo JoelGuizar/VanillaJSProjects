04075fb029fb5f0065a8a33d52c0f49a34052617
Joel Guizar <joelguizar@gmail.com>
2017-12-23 20:54:15 -0800

    slide-in-on-scroll rewrite

--- slide-in-on-scroll/index.js
+++ slide-in-on-scroll/index.js
@@ -1,35 +1,14 @@
 function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
-
-const sliderImages = document.querySelectorAll('.slide-in');
-
-function checkSlide(e){
-  sliderImages.forEach(sliderImage => {
-    //halfway through the image
-    const slideInAt = (window.scrollY - window.innerHeight) - sliderImage.height/2;
-    //bottom of the image
-    const imageBottom = sliderImage.offsetTop + sliderImage.height;
-    const isHalfShown = slideInAt > sliderImage.offsetTop;
-    const isNotScrolledPast = window.scrollY < imageBottom;
-    console.log(sliderImage);
-    if (isHalfShown && isNotScrolledPast) {
-      sliderImage.classList.add('active');
-    } else {
-      sliderImage.classList.remove('active');
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
     }
-  })
-}
-
-window.addEventListener('scroll', debounce(checkSlide))
